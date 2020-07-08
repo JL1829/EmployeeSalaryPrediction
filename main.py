58c73b9744e158eb67aa0cf70ef9c9f01e4d7071
JL1829 <johnnylou89@icloud.com>
2020-07-08 23:16:46 +0800

    modify main.py

--- main.py
+++ main.py
@@ -27,9 +27,50 @@ from lightgbm import LGBMRegressor
 
 #################################
 # self packages
-from src import preprocessing
-from src.features import build_features
-from src.models import predict_model
+from src.preprocessing import DataSetGenerator
+from src.features.build_features import FeatureGenerator
+from src.models.predict_model import ModelGenerator
+
+
+def print_summary(models, data):
+    '''prints summary of models, best model, and feature importance'''
+    print('\nModel Summaries:\n')
+    for model in models.mean_mse:
+        print('\n', model, '- MSE:', models.mean_mse[model])
+    print('\nBest Model:\n', models.best_model)
+    print('\nMSE of Best Model\n', models.mean_mse[models.best_model])
+    print('\nFeature Importances\n', models.get_feature_importance(models.best_model, data.feature_cols))
+
+    feature_importances = models.get_feature_importance(models.best_model, data.feature_cols)
+    feature_importances.plot.bar()
+    plt.show()    
+
+def main(train_feature_file:str, 
+         train_target_file:str, test_file:str, 
+         cat_cols:list, num_cols:list, target_col:str, id_col:str):
+    data = DataSetGenerator(train_feature_file, train_target_file, test_file, cat_cols, num_cols, target_col, id_col)
+    
+    feature_engineering = True
+
+    if feature_engineering:
+        FeatureGenerator(data).add_group_stats()
+    
+    models = ModelGenerator(models=[], data=data)
+
+    models.add_model(LinearRegression())
+    models.add_model(RandomForestRegressor(n_estimators=100, n_jobs=-1, max_depth=15, min_samples_split=80,
+                                           max_features=8))
+    models.add_model(GradientBoostingRegressor(n_estimators=100, max_depth=7))
+    models.add_model(XGBRegressor())
+    models.add_model(LGBMRegressor())
+    models.cross_validate()
+    models.select_best_model()
+
+    models.best_model_fit(data.train_df[data.feature_cols], data.train_df[data.target_col])
+    models.best_model_predict(data.test_df[data.feature_cols])
+    
+    print_summary(models, data)
+
 
 if __name__ == "__main__":
     # define input files
@@ -43,37 +84,4 @@ if __name__ == "__main__":
     target_col = 'salary'
     id_col = 'jobId'
 
-    data = preprocessing.DataSetGenerator(train_feature_file, train_target_file, test_file, cat_cols, num_cols, target_col, id_col)
-
-    feature_engineering = True
-
-    if feature_engineering:
-        feature_generator = build_features.FeatureGenerator(data)
-        feature_generator.add_group_stats()
-    
-    models = predict_model.ModelGenerator(models=[], data=data)
-
-    models.add_model(LinearRegression())
-    models.add_model(RandomForestRegressor(n_estimators=100, n_jobs=-1, max_depth=15, min_samples_split=80,
-                                           max_features=8))
-    models.add_model(GradientBoostingRegressor(n_estimators=100, max_depth=7, loss='ls'))
-    models.add_model(XGBRegressor())
-    models.add_model(LGBMRegressor())
-
-    models.select_best_model()
-
-    models.best_model_fit(data.train_df[data.feature_cols], data.train_df[data.target_col])
-    models.best_model_predict(data.test_df[data.feature_cols])
-
-def print_summary(models):
-    '''prints summary of models, best model, and feature importance'''
-    print('\nModel Summaries:\n')
-    for model in models.mean_mse:
-        print('\n', model, '- MSE:', models.mean_mse[model])
-    print('\nBest Model:\n', models.best_model)
-    print('\nMSE of Best Model\n', models.mean_mse[models.best_model])
-    print('\nFeature Importances\n', models.get_feature_importance(models.best_model, data.feature_cols))
-
-    feature_importances = models.get_feature_importance(models.best_model, data.feature_cols)
-    feature_importances.plot.bar()
-    plt.show()    
+    main(train_feature_file, train_target_file, test_file, cat_cols, num_cols, target_col, id_col)
